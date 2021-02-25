f3c30a2078e7d7f80f6743fa0cbfda9548ec7ac0
Alejandro Luque Estepa <aluque@iaa.es>
2021-02-25 16:42:46 +0100

    Implement inhomogeneous b.c.

--- src/boundaries.jl
+++ src/boundaries.jl
@@ -53,6 +53,13 @@ dim(::RightBnd) = 2
 dim(::FrontBnd) = 3
 dim(::BackBnd) = 3
 
+dirind(::BottomBnd) = -1
+dirind(::TopBnd) = 1
+dirind(::LeftBnd) = -1
+dirind(::RightBnd) = 1
+dirind(::FrontBnd) = -1
+dirind(::BackBnd) = 1
+
 targetind(rng, ::BottomBnd) = (first(rng) - 1, first(rng))
 targetind(rng, ::TopBnd)    = (last(rng)  + 1, last(rng))
 targetind(rng, ::LeftBnd)   = (first(rng) - 1, first(rng))
@@ -125,7 +132,27 @@ end
     out
 end
 
-
+"""
+Modifies the rhs of the Poisson equation to satisfy an inhomogeneous boundary 
+condition with independent term in array `val`.  Note that if you want
+Neumann inhomogeneous condition to the derivative of ϕ you should include
+a factor h in `val`.
+"""
+function setinhom!(conf::MGConfig,
+                   b::AbstractArray{T, N},
+                   bnd::AbstractBoundary,
+                   val::AbstractArray) where {T, N}
+    @unpack s, g, conn = conf
+    v = valid(g, b, bnd)
+    
+    z = zero(CartesianIndex{N})
+    I = Base.setindex(z, v.indices[dim(bnd)], dim(bnd))
+    D = Base.setindex(z, dirind(bnd), dim(bnd))
+    @show conn(g, I, D, 2 / s)
+    
+    v .+= conn(g, I, D, 2 / s) .* val
+end
+    
 @inline function matcoef(rngs, ind, b::AbstractBoundary,
                          c::AbstractLinearCondition)
     i1, i2 = targetind(rngs[dim(b)], b)
